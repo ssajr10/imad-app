137d93bd8213fdf55b78d07e8880573ffdad31b4
Syed Salman Ashraf <36541764+ssajr10@users.noreply.github.com>
2018-02-20 19:04:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='New value';
+
+
+var img=document.getElementById('img');
+img.onClick=function () {
+    img.style.marginLeft='100px';
+};
