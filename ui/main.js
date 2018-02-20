9c8169a0ce4cbeb85397ac73a688f35df92b61de
Syed Salman Ashraf <36541764+ssajr10@users.noreply.github.com>
2018-02-20 20:56:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,6 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-
-element.innerHTML='Welcome';
-
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft+=1;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick=function () {
-    var interval= setInterval(moveRight,50);
-    
+var button= document.getElementId('counter');
+button.onclick = function(){
+    counter++;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
