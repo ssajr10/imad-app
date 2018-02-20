30b4ffed104601c477aef639193b1559b02bf939
Syed Salman Ashraf <36541764+ssajr10@users.noreply.github.com>
2018-02-20 14:31:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var aticleName=req.params.articleName;
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
