a7ea7931e0bec1c9c3cc60d08fbeda2fae343644
Syed Salman Ashraf <36541764+ssajr10@users.noreply.github.com>
2018-02-20 19:15:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ element.innerHTML='New value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight () {
-    marginLeft+=10;
+    marginLeft+=5;
     img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function () {
-    var interval= setInterval(moveRight,100);
+    var interval= setInterval(moveRight,50);
     
 };
