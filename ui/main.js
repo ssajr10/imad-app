11711924d8ddf9a9219049957968b09ace1d5f57
Syed Salman Ashraf <36541764+ssajr10@users.noreply.github.com>
2018-02-21 19:13:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ var button= document.getElementById('counter');
 
 button.onclick = function(){
     
-    var request=new XMLHttpRequest();
-    request.onreadystatechange= function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
         
         if (request.readyState === XMLHttpRequest.DONE) {
-            if(request.status===200){
+            if(request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML=counter.toString();
@@ -15,7 +15,7 @@ button.onclick = function(){
         
     };
     
-   request.open('GET','http://salmanashrafsyed.imad.hasura-app.io/',true);
+   request.open('GET','http://salmanashrafsyed.imad.hasura-app.io/counter',true);
    request.send(null);
     
 };
